c18ddc00385a7ebb4b7e42cf338de44fe19dd3e9
Eryk Jalowiec <eryk.jalowiec@gmail.com>
2020-11-19 16:30:37 +0100

    added reading from the file

--- aproksymacja.cpp
+++ aproksymacja.cpp
@@ -1,9 +1,61 @@
-﻿#include <iostream>
-
+﻿// https://github.com/ejalowiec/nm-approximation
+#include <iostream>
+#include <fstream>
+#include <mkl_lapack.h>
 
 
 using namespace std;
 
 int main() {
-    std::cout << "Hello World!\n";
+
+	// ---------------------- reading file ----------------------
+
+	fstream file;
+	file.open("ex1.txt", ios::in);
+
+	if (!file.good()) {
+		cerr << "File does not exist";
+		exit(0);
+	}
+
+	int size;
+	file >> size;
+
+	if (size <= 1) {
+		cerr << "Number of nodes is too low";
+		exit(0);
+	}
+
+	double** arr = new double*[size];
+	for (int i = 0; i < size; ++i)
+		arr[i] = new double[2];
+
+	for (int i = 0; i < size; ++i) {
+		for (int j = 0; j < 2; ++j)
+			file >> arr[i][j];
+	}
+
+	file.close();
+
+	cout << size << endl;
+
+	// display read file
+
+	//for (int i = 0; i < size; ++i) {
+	//	for (int j = 0; j < 2; ++j) {
+	//		cout << arr[i][j] << " ";
+	//		if (j == 1)
+	//			cout << "\n";
+	//	}
+	//}
+
+
+
+
+
+
+
+
+
+
 }
